4fb66cd2508e5722dea1e2e0be3f83135207fe97
Luis <luispaulo.ni@gmail.com>
2024-10-11 17:10:42 -0300

    removed headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,5 @@
 import withBundleAnalyzer from '@next/bundle-analyzer'
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://authjs.dev;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self' https://accounts.google.com;
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-    connect-src 'self' http://localhost:4000 https://accounts.google.com;
-`
-
 const bundleAnalyzer = withBundleAnalyzer({
     enabled: process.env.ANALYZE === 'true',
 })
@@ -21,35 +7,6 @@ const bundleAnalyzer = withBundleAnalyzer({
 const nextConfig = {
     compiler: {
         styledComponents: true,
-    },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'Content-Security-Policy',
-                        value: cspHeader.replace(/\n/g, ''),
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'Referrer-Policy',
-                        value: 'no-referrer',
-                    },
-                    {
-                        key: 'Permissions-Policy',
-                        value: 'geolocation=(*), microphone=(), camera=()',
-                    },
-                ],
-            },
-        ]
     },
 }
 
